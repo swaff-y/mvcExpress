6cfa8f22c5ffb831587ea6f4dd79e30ec08a0bd7
swaff-y <kyle@swaff.id.au>
2022-01-13 21:32:47 +1100

    Edit page

--- controllers/authors.js
+++ controllers/authors.js
@@ -52,8 +52,13 @@ router.get('/:id',(req, res)=>{
   res.send("Show Author" + req.params.id)
 })
 //edit one author
-router.get('/:id/edit',(req, res)=>{
-  res.send("Edit Author" + req.params.id)
+router.get('/:id/edit', async (req, res)=>{
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render("authors/edit", { author });
+  } catch {
+    res.redirect('/authors');
+  }
 })
 //update one author
 router.put('/:id',(req, res)=>{
