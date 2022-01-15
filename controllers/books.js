fe3a02c3c3cd55f882097b172c98765e2fd1a841
swaff-y <kyle@swaff.id.au>
2022-01-15 21:50:45 +1100

    Delete books - 19:37 10

--- controllers/books.js
+++ controllers/books.js
@@ -54,8 +54,7 @@ router.post('/', async (req, res)=>{
 
   try{
     const newBook = await book.save();
-    // res.redirect(`books/${newBook.id}`);
-    res.redirect(`books`);
+    res.redirect(`books/${newBook.id}`);
   }catch{
     renderFormPage(res, book, "new",  true);
   }
@@ -82,16 +81,55 @@ router.get('/:id/edit', async (req, res)=>{
   }
 });
 
+//Update
+router.put('/:id', async (req, res)=>{
+  let book;
+
+  try{
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publishedDate = new Date(req.body.publishedDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+    if(req.body.cover){
+      saveCover(book, req.body.cover)
+    }
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  }catch (err){
+    if(book){
+      renderFormPage(res, book, "edit",  true);
+    } else {
+      res.redirect(`/`);
+    }
+
+  }
+});
+
+//delete
+router.delete('/:id', async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    await book.remove();
+    res.redirect("/books");
+  } catch (err){
+    if(book){
+      res.render("books/show", { book, errorMessage: "Could not remove book"});
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
 async function renderFormPage(res, book, form, hasError = false) {
-  console.log("renderForm");
   try {
     const authors = await Author.find();
-    const params = {
-      authors: authors,
-      book: book
-    }
+    const params = { authors, book }
     // const book = new Book();
-    if(hasError) params.errorMessage = "Error creating book";
+    if(hasError && (form === "edit")) params.errorMessage = "Error updating book";
+    if(hasError && (form === "new")) params.errorMessage = "Error creating book";
     res.render(`books/${form}`, params)
   } catch(err) {
     res.redirect("/books")
